dd242c94cb2c0df4028e72d3e2ddd80d7cae7b72
sadosdesperados <bms39rus@gmail.com>
2019-10-03 14:09:20 +0200

    (Added filter by leaderCode to Client|Grid)

--- src/main/java/net/mike/views/ClientGrid.java
+++ src/main/java/net/mike/views/ClientGrid.java
@@ -2,8 +2,12 @@ package net.mike.views;
 
 import java.util.List;
 
+import com.vaadin.flow.component.HasValue;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.Route;
 
 import net.mike.entities.AutoPart;
@@ -12,13 +16,30 @@ import net.mike.services.AutoPartServices;
 @Route("grid")
 public class ClientGrid extends VerticalLayout {
 	
-	public ClientGrid() {
-		   AutoPartServices request = new AutoPartServices();
-	       List<AutoPart> priceList = request.getAllAutoParts();
-	        Grid<AutoPart> wrapper = new Grid<>(AutoPart.class);
-	        	wrapper.setItems(priceList);
-	        	setSizeFull();
-	        	add(wrapper);
-	}
+    private TextField leaderCodeFilter;
+	private Grid<AutoPart> wrapper;
+	private AutoPartServices request;
+	private List<AutoPart> priceList;
+		
+		public ClientGrid() {
+			request = new AutoPartServices();
+		   	priceList = request.getAllAutoParts();
+	       	wrapper = new Grid<>(AutoPart.class);
+	    	leaderCodeFilter = new TextField();
+	       	leaderCodeFilter.setPlaceholder("Enter part number");
+	       	leaderCodeFilter.addValueChangeListener(this::onNameFilterTextChange);
+	       	wrapper.setItems(priceList);
+	       		setSizeFull();
+	       		add(leaderCodeFilter, wrapper);
+		}
+	
+		private void onNameFilterTextChange(HasValue.ValueChangeEvent<String> event) {
+			ListDataProvider<AutoPart> dataProvider = (ListDataProvider<AutoPart>) wrapper.getDataProvider();
+			dataProvider.setFilter(AutoPart :: getLeaderCode, s -> caseInsensitiveContains(s, event.getValue()));
+		}
+
+    private Boolean caseInsensitiveContains(String where, String what) {
+        return where.toLowerCase().contains(what.toLowerCase());
+    }
 
 }
